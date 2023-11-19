13247baf24cba47cf8c29cf2e5be8036c361f09e
Evan Keller <evankeller@pop-os.localdomain>
2023-11-18 20:00:38 -0700

    inserted base code from develop folder, got background changer working

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,47 @@
-var dateTime = dayjs().format("MMM D, YYYY [at] hh:mm a");
-$("#dateTime").text(dateTime);
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(document).ready(function () {
+    // TODO: Add a listener for click events on the save button. This code should
+    // use the id in the containing time-block as a key to save the user input in
+    // local storage. HINT: What does `this` reference in the click listener
+    // function? How can DOM traversal be used to get the "hour-x" id of the
+    // time-block containing the button that was clicked? How might the id be
+    // useful when saving the description in local storage?
+
+
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+
+    //Fetches the current hour of the day
+    var timeOfDay = dayjs().format('[hour-]HH')
+    console.log('Current hour: ' + timeOfDay)
+
+    //Compares the current hour of the day to each planner div and applies a class to each
+    $('.time-block').each(function () {
+        var domTimeBlock = $(this).attr('id')
+        console.log(domTimeBlock)
+        if (timeOfDay === domTimeBlock) {
+            $(this).addClass('present')
+        } else if (timeOfDay > domTimeBlock) {
+            $(this).addClass('past')
+        } else {
+            $(this).addClass('future')
+        }
+    })
+
+
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+
+
+
+
+    // TODO: Add code to display the current date in the header of the page.
+    var dateTime = dayjs().format("MMM D, YYYY [at] hh:mm a");
+    $("#currentDay").text(dateTime);
+});
